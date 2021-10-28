4dea546a0519d52dbd0aab0cd75522cf7753da74
fikerte <tofikertesfaye@gmail.com>
2021-10-28 10:21:46 +0300

    Mob programming: FizzBuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,16 @@
 module.exports = function () {
   // write your code here
+  let array = [];
+  let num = 100;
+  for(let i =1; i<=100; i++) {
+    if(i%5===0 && i%3===0){
+      array.push("FizzBuzz");
+    }else if(i%5===0){
+      array.push("Buzz");
+    }else if(i%3===0){
+      array.push("Fizz");
+    }else 
+      array.push(i);
+  }
+return array;
 }
